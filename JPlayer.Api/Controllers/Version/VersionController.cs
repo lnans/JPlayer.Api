ca3903f187a55f7f803d58916f7889ea415ceb71
Laupretre Nans <nans.laupretre@gmail.com>
2020-11-06 20:11:53 +0100

    Create Version Controller

---
+++ JPlayer.Api/Controllers/Version/VersionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JPlayer.Api.Controllers.Version
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        /// <summary>
+        ///     Get the version of the current application
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("")]
+        public IActionResult GetVersion()
+        {
+            System.Version assemblyVer = typeof(Program).Assembly.GetName().Version;
+            if (assemblyVer != null)
+            {
+                string version = $"{assemblyVer.Major}.{assemblyVer.Minor}.{assemblyVer.Build}";
+                return this.Ok(version);
+            }
+
+            return this.StatusCode(500);
+        }
+    }
+}
--- JPlayer.Api/Startup.cs
+++ JPlayer.Api/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +11,7 @@ namespace JPlayer.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,7 +22,7 @@ namespace JPlayer.Api
                 app.UseDeveloperExceptionPage();
 
             app.UseRouting();
-            app.UseEndpoints(endpoints => { endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); }); });
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
