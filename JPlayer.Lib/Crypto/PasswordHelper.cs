04d04c861f68c2452a8f80afc6b8e541fa183c72
Laupretre Nans <nans.laupretre@gmail.com>
2020-11-08 21:22:48 +0100

    Create PasswordHelper class

---
+++ JPlayer.Lib/Crypto/PasswordHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace JPlayer.Lib.Crypto
+{
+    public static class PasswordHelper
+    {
+        public static string Crypt(string salt, string password)
+        {
+            string saltedPassword = salt + "#" + password;
+            return Sha512(saltedPassword);
+        }
+
+        public static bool Check(string salt, string givenPassword, string cryptedPassword) => Crypt(salt, givenPassword) == cryptedPassword;
+
+        private static string Sha512(string value)
+        {
+            using (SHA512 hash = SHA512.Create())
+            {
+                StringBuilder res = new StringBuilder();
+                byte[] result = hash.ComputeHash(Encoding.UTF8.GetBytes(value));
+                foreach (byte b in result)
+                    res.Append(b.ToString("x2"));
+
+                return res.ToString();
+            }
+        }
+    }
+}
