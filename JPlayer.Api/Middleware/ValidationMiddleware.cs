eac3fb284825f60d6167b6c12f6e47fea5bb0385
Laupretre Nans <nans.laupretre@gmail.com>
2020-11-10 22:27:28 +0100

    Add logs on validation model errors

--- JPlayer.Api/Middleware/ValidationMiddleware.cs
+++ JPlayer.Api/Middleware/ValidationMiddleware.cs
@@ -6,11 +6,15 @@ using JPlayer.Business;
 using JPlayer.Lib.Contract;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace JPlayer.Api.Middleware
 {
     public class ValidationMiddleware : IActionResult
     {
+        private readonly ILogger<ValidationMiddleware> _logger = new Logger<ValidationMiddleware>(new NLogLoggerFactory());
+
         public async Task ExecuteResultAsync(ActionContext context)
         {
             KeyValuePair<string, ModelStateEntry>[] modelStateEntries = context.ModelState.Where(e => e.Value.Errors.Count > 0).ToArray();
@@ -21,6 +25,10 @@ namespace JPlayer.Api.Middleware
                 Error = GlobalLabelCodes.RequestValidationError,
                 Details = errors.Select(err => err.ErrorMessage)
             };
+
+            this._logger.LogInformation(GlobalLabelCodes.RequestValidationError);
+            foreach (string errorDetail in error.Details)
+                this._logger.LogInformation(errorDetail);
 
             context.HttpContext.Response.StatusCode = 400;
             context.HttpContext.Response.ContentType = "application/json";
