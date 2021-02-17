5cc71301c73b16dc5cdc7cff10f2ae9935721687
Nans Laupretre <nans.laupretre@gmail.com>
2021-02-17 19:42:48 +0100

    Use snake case for GlobalLabelCodes

--- JPlayer.Business/GlobalLabelCodes.cs
+++ JPlayer.Business/GlobalLabelCodes.cs
@@ -2,27 +2,27 @@
 {
     public static class GlobalLabelCodes
     {
-        public const string RequestValidationError = "requestValidationError";
+        public const string RequestValidationError = "request_validation_error";
         
-        public const string AuthAuthenticationFailed = "authAuthenticationFailed";
-        public const string AuthWrongPassword = "authWrongPassword";
-        public const string AuthNotAuthenticated = "authNotAuthenticated";
-        public const string AuthNotAuthorized = "authNotAuthorized";
+        public const string AuthAuthenticationFailed = "auth_authentication_failed";
+        public const string AuthWrongPassword = "auth_wrong_password";
+        public const string AuthNotAuthenticated = "auth_not_authenticated";
+        public const string AuthNotAuthorized = "auth_not_authorized";
 
-        public const string UserNotFound = "userNotFound";
-        public const string UserAlreadyExist = "userAlreadyExist";
-        public const string UserReadOnly = "userReadOnly";
-        public const string UserTileLabel = "userTileLabel";
-        public const string UserTileSubLabel = "userTileSubLabel";
+        public const string UserNotFound = "user_not_found";
+        public const string UserAlreadyExist = "user_already_exist";
+        public const string UserReadOnly = "user_read_only";
+        public const string UserTileLabel = "user_tile_label";
+        public const string UserTileSubLabel = "user_tile_sub_label";
 
-        public const string ProfileNotFound = "profileNotFound";
-        public const string ProfileAlreadyExist = "profileAlreadyExist";
-        public const string ProfileReadOnly = "profileReadOnly";
-        public const string ProfileTileLabel = "profileTileLabel";
-        public const string ProfileTileSubLabel = "profileTileSubLabel";
+        public const string ProfileNotFound = "profile_not_nound";
+        public const string ProfileAlreadyExist = "profile_already_exist";
+        public const string ProfileReadOnly = "profile_read_only";
+        public const string ProfileTileLabel = "profile_tile_label";
+        public const string ProfileTileSubLabel = "profile_tile_sub_label";
 
-        public const string FunctionNotFound = "functionNotFound";
-        public const string FunctionTileLabel = "functionTileLabel";
-        public const string FunctionTileSubLabel = "functionTileSubLabel";
+        public const string FunctionNotFound = "function_not_found";
+        public const string FunctionTileLabel = "function_tile_label";
+        public const string FunctionTileSubLabel = "function_tile_sub_label";
     }
 }
